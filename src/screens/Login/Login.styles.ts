c76464c70fcc12d096dca746b0b7897f87b44965
Vinicius Pinheiro <vinicius.pinheiro@wisereducacao.com>
2021-02-23 01:24:10 -0300

    feat: add core login

---
+++ src/screens/Login/Login.styles.ts
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #130525;
+  position: relative;
+
+  .main {
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
+    height: 100vh;
+    z-index: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &__content {
+      width: 311px;
+      height: 357px;
+
+      &__data {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        width: 311px;
+        height: 357px;
+        background-color: #faf5ff;
+        border: 1px solid #faf5ff;
+        border-radius: 8px;
+
+        h1 {
+          font-size: 24px;
+          line-height: 32px;
+          width: 139px;
+          text-align: center;
+          color: #383e71;
+        }
+
+        h4 {
+          font-size: 12px;
+          line-height: 20px;
+          text-align: center;
+          width: 255;
+          color: #989fdb;
+          margin: 16px 0 20px 0;
+        }
+      }
+    }
+  }
+`;
+
+const BgImage = styled.div`
+  width: 100%;
+  height: 60vh;
+  position: relative;
+  background-image: url('./bg-mobile.jpeg');
+  background-position: center;
+  background-size: cover;
+
+  &:after {
+    position: absolute;
+    content: '';
+    height: 100%;
+    width: 100%;
+    top: 0;
+    left: 0;
+    background: linear-gradient(#69399900, #130525);
+  }
+`;
+
+export { BgImage, Wrapper };
--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import Head from 'next/head';
+
+// Styles
+import { BgImage, Wrapper } from './Login.styles';
 
 const Login: React.FC = () => {
   return (
-    <div>
-      <Head>
-        <title>Login</title>
-      </Head>
-
-      <main>
-        <h1>Login</h1>
-      </main>
-    </div>
+    <Wrapper>
+      <div className="main">
+        <div className="main__content">
+          <div className="main__content__data">
+            <h1>Olá, seja bem-vindo!</h1>
+            <h4>Para acessar a plataform, faça seu login.</h4>
+            <form className="form">
+              <div className="form__input">
+                <label htmlFor="email">
+                  <input type="email" id="email" />
+                </label>
+              </div>
+              <div className="form__input">
+                <label htmlFor="password">
+                  <input type="password" id="password" />
+                </label>
+              </div>
+            </form>
+            <button type="submit">entrar</button>
+          </div>
+          <h3>Esqueceu seu login ou senha?</h3>
+          <h3>Clique aqui</h3>
+        </div>
+      </div>
+      <BgImage />
+    </Wrapper>
   );
 };
 
